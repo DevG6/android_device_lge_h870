d2a9090dc54099b589b1c497a39033845ae71924
Eliminater74 <eliminater74@gmail.com>
2018-08-13 20:13:24 -0400

    [twrp] [h870] Fix Up Device Tree:
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- Android.mk
+++ Android.mk
@@ -13,26 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# common g6
-$(call inherit-product, device/lge/g6-common/g6.mk)
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product-if-exists, vendor/lge/h870/h870-vendor.mk)
+LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := device/lge/h870/kernel
-else
-	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+ifneq ($(filter h870,$(TARGET_DEVICE)),)
+include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
-
-
-PRODUCT_DEVICE := h870
-PRODUCT_NAME := omni_h870
-PRODUCT_BRAND := lge
-PRODUCT_MODEL := LG-H870
-PRODUCT_MANUFACTURER := LGE
-
